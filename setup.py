f78786a50d128b52584c662a9c85f934418ed6d3
Hervé Beraud <herveberaud.pro@gmail.com>
2017-12-06 18:16:56 +0100

    refactor python packaging

--- setup.py
+++ setup.py
@@ -131,37 +131,5 @@ try:
     ### end cx_Freeze ###
 except ImportError:
     setup(
-        name = "elasticsearch-curator",
-        version = get_version(),
-        author = "Elastic",
-        author_email = "info@elastic.co",
-        description = "Tending your Elasticsearch indices",
-        long_description=fread('README.rst'),
-        url = "http://github.com/elastic/curator",
-        download_url = "https://github.com/elastic/curator/tarball/v" + get_version(),
-        license = "Apache License, Version 2.0",
-        install_requires = get_install_requires(),
-        keywords = "elasticsearch time-series indexed index-expiry",
-        packages = ["curator"],
-        include_package_data=True,
-        entry_points = {
-            "console_scripts" : [
-                "curator = curator.cli:cli",
-                "curator_cli = curator.curator_cli:main",
-                "es_repo_mgr = curator.repomgrcli:repo_mgr_cli",
-            ]
-        },
-        classifiers=[
-            "Intended Audience :: Developers",
-            "Intended Audience :: System Administrators",
-            "License :: OSI Approved :: Apache Software License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3.4",
-            "Programming Language :: Python :: 3.5",
-            "Programming Language :: Python :: 3.6",
-        ],
         test_suite = "test.run_tests.run_all",
-        tests_require = ["mock", "nose", "coverage", "nosexcover"]
     )
